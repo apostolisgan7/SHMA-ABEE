f2b78276d7736521dbc1eeac1bf12befcf722ed5
apostolisgan7 <apostolisgan@gmail.com>
2025-12-03 16:39:38 +0200

    fixes

--- single-service.php
+++ single-service.php
@@ -4,6 +4,7 @@ get_header(); ?>
 <div class="single_services_page">
     <?php get_template_part('template-parts/services/service-hero'); ?>
     <?php get_template_part('template-parts/services/service-characteristics'); ?>
+    <?php get_template_part('template-parts/services/service-fullscreen-content'); ?>
     <?php get_template_part('template-parts/services/service-faq'); ?>
     <?php get_template_part('template-parts/services/service-products'); ?>
     <?php get_template_part('template-parts/services/service-banner'); ?>

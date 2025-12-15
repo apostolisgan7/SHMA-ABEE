22cd064b7b1bdc07cd72f698bcea95ec801360c8
apostolisgan7 <apostolisgan@gmail.com>
2025-12-15 18:53:43 +0200

    contact page

--- src/js/utils/contact-form.js
+++ src/js/utils/contact-form.js
@@ -1,3 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.rv-cf7__submit button br').forEach(br => br.remove());
 });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+
+    function markFields(event) {
+        if (!event.target) return;
+
+        const fields = event.target.querySelectorAll(
+            'input[type="text"], input[type="email"], input[type="tel"], textarea'
+        );
+
+        fields.forEach(el => {
+            el.classList.remove('is-valid');
+
+            if (
+                !el.classList.contains('wpcf7-not-valid') &&
+                el.value.trim() !== ''
+            ) {
+                el.classList.add('is-valid');
+            }
+        });
+    }
+
+    document.addEventListener('wpcf7invalid', markFields);
+    document.addEventListener('wpcf7submit', markFields);
+
+});

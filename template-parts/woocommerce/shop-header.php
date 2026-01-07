cc7ad872da8984d6a0e56676e956e75f3d3d7d82
apostolisgan7 <apostolisgan@gmail.com>
2026-01-07 19:25:29 +0200

    fixed filter and mobile list

--- template-parts/woocommerce/shop-header.php
+++ template-parts/woocommerce/shop-header.php
@@ -11,7 +11,7 @@
         <!-- Toggle Filters (desktop) -->
         <button
                 class="archive-header__filters"
-                @click="toggleFilters"
+                @click="handleFiltersClick"
         >
             <span x-text="filtersLabel"></span>
             <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">

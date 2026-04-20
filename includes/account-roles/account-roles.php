4d05c0d0508c58ee53fa4c0e0aa56e388c0d03d6
apostolisgan7 <apostolisgan@gmail.com>
2026-04-20 13:28:32 +0300

    css fixes

--- includes/account-roles/account-roles.php
+++ includes/account-roles/account-roles.php
@@ -9,5 +9,4 @@ require_once __DIR__ . '/admin-user-fields.php';
 require_once __DIR__ . '/auth/ajax-login.php';
 require_once __DIR__ . '/auth/ajax-register.php';
 require_once __DIR__ . '/auth/ajax-check-vat.php';
-require_once __DIR__ . '/vat-validation.php';
 
